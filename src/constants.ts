1e10bd92371a40fc3236bc7c50923493118a00e2
Yarovenko Dmitry <yarovenkodmv@gmail.com>
2022-05-13 14:53:12 +0300

    android fix: fix shifted background

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,6 @@ export const { height, width } = Dimensions.get('window');
 
 export const SPACING = 10;
 export const isIos = Platform.OS === 'ios';
-export const ITEM_SIZE = isIos ? width * 0.718 : width * 0.74;
+export const ITEM_SIZE = isIos ? width * 0.718 : width * 0.7;
 export const EMPTY_ITEM_SIZE = (width - ITEM_SIZE) / 2;
 export const BACKDROP_HEIGHT = height * 0.65;
